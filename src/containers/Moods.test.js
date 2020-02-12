6863c3dd2a2052499102756513b33781944d3e8c
Joseph Tatum <josephtatum@gmail.com>
2020-02-12 15:56:16 -0800

    removed unnecessary tests. all tests passing

--- src/containers/Moods.test.js
+++ src/containers/Moods.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Moods, { isTired, isHyper, isEducated, isHungry, getFace } from './Moods';
+import Moods, { isTired, isHyper, isEducated, isHungry, getFace } from '../selectors/moodSelectors';
 
 describe('Moods', () => {
   describe('mood selectors', () => {
@@ -62,41 +62,6 @@ describe('Moods', () => {
 
     it('is happy if not tired, hungry, educated, or hungry', () => {
       expect(getFace({ coffees: 1, naps: 0, snacks: 1, studies: 0 })).toEqual('😀');
-    });
-  });
-
-  describe('container', () => {
-    it('matches a snapshot', () => {
-      const wrapper = shallow(<Moods />);
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('updates state on DRINK_COFFEE selection', () => {
-      const wrapper = shallow(<Moods />);
-      wrapper.instance().handleSelection('DRINK_COFFEE');
-
-      expect(wrapper.state('coffees')).toEqual(1);
-    });
-
-    it('updates state on TAKE_NAP selection', () => {
-      const wrapper = shallow(<Moods />);
-      wrapper.instance().handleSelection('TAKE_NAP');
-
-      expect(wrapper.state('naps')).toEqual(1);
-    });
-
-    it('updates state on EAT_SNACK selection', () => {
-      const wrapper = shallow(<Moods />);
-      wrapper.instance().handleSelection('EAT_SNACK');
-
-      expect(wrapper.state('snacks')).toEqual(1);
-    });
-
-    it('updates state on STUDY selection', () => {
-      const wrapper = shallow(<Moods />);
-      wrapper.instance().handleSelection('STUDY');
-
-      expect(wrapper.state('studies')).toEqual(1);
     });
   });
 });
